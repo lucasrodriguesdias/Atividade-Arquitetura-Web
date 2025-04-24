0f4509c45dedada511314309902763700dd44f4d
Lucas Nascimento <145167515+lucasrodriguesdias@users.noreply.github.com>
2025-04-24 20:57:47 -0300

    Add files via upload

---
+++ terceiroprj/src/main/java/com/mvc/tarefas/repository/TarefaRepository.java
@@ -0,0 +1,7 @@
+package com.mvc.tarefas.repository;
+
+import com.mvc.tarefas.model.Tarefa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
